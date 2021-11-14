9856447f35d2b3c0f7c0432f509f5b086345af9b
sindhuja shukla <92431196+sindhu188@users.noreply.github.com>
2021-11-14 15:20:22 +0530

    Add files via upload

---
+++ printZ.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Write your code here
+    Scanner scn = new Scanner(System.in);
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+
+  }
+}
